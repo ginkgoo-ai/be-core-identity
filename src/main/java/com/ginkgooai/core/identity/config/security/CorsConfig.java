6e9273ddca09917345208d1e73c5154baad7cc31
bruce <bruce.zhang@biteinvestments.com>
2025-02-14 13:15:45 +0800

    feature:Support internal&external domain cors

--- src/main/java/com/ginkgooai/core/identity/config/security/CorsConfig.java
+++ src/main/java/com/ginkgooai/core/identity/config/security/CorsConfig.java
@@ -1,8 +1,11 @@
 package com.ginkgooai.core.identity.config.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,8 +16,12 @@ import java.util.List;
 
 @Configuration
 @ConfigurationProperties(prefix = "security.cors")
+@Slf4j
 public class CorsConfig {
     private List<String> allowedOrigins;
+
+    @Value("${app.dev-uris}")
+    private String appDevUrls;
 
     public List<String> getAllowedOrigins() {
         return allowedOrigins;
@@ -29,6 +36,15 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         List<String> origins = new ArrayList<>(allowedOrigins);
         origins.add("null");
+        if (!ObjectUtils.isEmpty(appDevUrls)) {
+            origins.addAll(
+                    Arrays.stream(appDevUrls.split(","))
+                            .map(String::trim)
+                            .filter(url -> !url.isEmpty())
+                            .toList()
+            );
+        }
+        log.debug("Allowed origins: {}", origins);
         config.setAllowedOrigins(origins);
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));

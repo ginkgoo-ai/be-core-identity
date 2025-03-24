c9f66db8f2c5ef5cca79eed4d750b128b42af3b7
seasonsolt <seasonsolt@gmail.com>
2025-03-24 14:11:56 +0800

    feature:redis session

---
+++ src/main/java/com/ginkgooai/core/identity/config/security/SessionConfig.java
@@ -0,0 +1,37 @@
+package com.ginkgooai.core.identity.config.security;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+
+@Configuration
+@EnableSpringHttpSession
+public class SessionConfig {
+    
+    @Value("${app.domain-name}")
+    private String domainName;
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("None");
+        // Set to true if using HTTPS
+        serializer.setUseSecureCookie(true);
+//        serializer.setDomainName(domainName);
+        serializer.setCookiePath("/");
+        return serializer;
+    }
+
+    @Bean
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        CookieHttpSessionIdResolver resolver = new CookieHttpSessionIdResolver();
+        resolver.setCookieSerializer(cookieSerializer());
+        return resolver;
+    }
+}
